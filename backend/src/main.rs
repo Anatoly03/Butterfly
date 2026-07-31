134c435a9927b59d27a12b2f7a245596f72ecb28
Anatoly Wein <anatoly.wein@gmail.com>
2026-07-31 21:55:09 +0200

    add dotenv

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -10,10 +10,15 @@ use axum::{
 /// [axum] web server.
 #[tokio::main]
 async fn main() {
+    dotenv::dotenv().ok();
+
     // build our application with a single route
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
-    // run our app with hyper, listening globally on port 3000
+    // run our app with hyper, listening globally on specified port
+    let hostname = format!("0.0.0.0:{}", dotenv::var("PORT").unwrap_or("3000".to_string()));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Listening on {hostname}");
+
     axum::serve(listener, app).await.unwrap();
 }
