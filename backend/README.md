3075aff8d224bde18ed887d42ced1e115b2f640e
Anatoly Wein <anatoly.wein@gmail.com>
2026-07-31 22:27:41 +0200

    add database

--- backend/README.md
+++ backend/README.md
@@ -1,3 +1,15 @@
 # Butterfly API
 
 API and server component for the Butterfly project.
+
+### Building and Running
+
+- TBA
+
+### Migrating Database
+
+First install [cargo-binstall](https://docs.rs/crate/cargo-binstall/latest) and then install the `sqlx` command with the following command to access sqlx commands.
+
+```sh
+cargo binstall sqlx-cli
+```
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,24 +1,31 @@
 //! The entry point of the Butterfly backend application. This module starts the
 //! [axum] web server.
 
-use axum::{
-    routing::get,
-    Router,
-};
+use axum::{Router, routing::get};
+use sqlx::{Error, SqlitePool, sqlite::SqliteConnectOptions};
+use std::{str::FromStr, time::Duration};
 
 /// The entry point of the Butterfly backend application. This function starts the
 /// [axum] web server.
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
+    std::fs::create_dir_all(".butterfly").unwrap();
 
     // build our application with a single route
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
     // run our app with hyper, listening globally on specified port
-    let hostname = format!("0.0.0.0:{}", dotenv::var("PORT").unwrap_or("3000".to_string()));
+    let port = dotenv::var("PORT").unwrap_or("3000".to_string());
+    let hostname = format!("0.0.0.0:{port}");
     let listener = tokio::net::TcpListener::bind(&hostname).await.unwrap();
     println!("Listening on {hostname}");
+
+    // production ready sqlx pool configuration
+    let options = SqliteConnectOptions::from_str("sqlite://.butterfly/data.db")
+        .unwrap()
+        .create_if_missing(true);
+    let pool = SqlitePool::connect_with(options).await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
 }
